39f3dc0b07e343cd4a8073b7b02a6cc6b1d8b000
dev.bhuptani <devbhuptani.jarvis@gmail.com>
2026-05-20 17:07:35 +0530

    feat: database connectivity changes

--- config/env/development/database.ts
+++ config/env/development/database.ts
@@ -8,22 +8,17 @@ export default ({ env }) => {
             connection: {
                 host: env("DATABASE_HOST", "127.0.0.1"),
                 port: env.int("DATABASE_PORT", 5432),
+
                 database: env("DATABASE_NAME", "strapi_dev"),
-                user: env("DATABASE_USERNAME", "postgres"),
-                password: env("DATABASE_PASSWORD", "postgres"),
-                ssl: env.bool("DATABASE_SSL", false)
-                    ? {
-                        rejectUnauthorized: env.bool(
-                            "DATABASE_SSL_SELF",
-                            false
-                        ),
-                    }
-                    : false,
+                user: env("DATABASE_USERNAME"),
+                password: env("DATABASE_PASSWORD"),
+                
+                ssl: env.bool("DATABASE_SSL", false),
                 schema: env("DATABASE_SCHEMA", "public"),
             },
             pool: {
-                min: env.int("DATABASE_POOL_MIN", 2),
-                max: env.int("DATABASE_POOL_MAX", 10),
+                min: 0,
+                max: 10,
             },
         },
 
