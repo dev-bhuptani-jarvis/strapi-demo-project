c7e660e861a6112bd7dfda2443b74285d837c3f2
dev.bhuptani <devbhuptani.jarvis@gmail.com>
2026-05-21 10:14:38 +0530

    feat: build issue

--- config/admin.ts
+++ config/admin.ts
@@ -1,23 +1,35 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Admin => ({
+const config = ({ env }: Core.Config.Shared.ConfigParams) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
+
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
+
   transfer: {
     token: {
       salt: env('TRANSFER_TOKEN_SALT'),
     },
   },
+
   secrets: {
     encryptionKey: env('ENCRYPTION_KEY'),
   },
+
   flags: {
     nps: env.bool('FLAG_NPS', false),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  },
+
+  vite: {
+    server: {
+      allowedHosts: [
+        'admin-integriticonsulting.jprojects.in',
+      ],
+    },
   },
 });
 
